7eeefed765c1143fd998d47a86840bd49a9b77e0
ts-oh <this.tsoh@gmail.com>
2023-07-13 14:20:00 +0900

    :bento: [ADD]: movie helper and flop instance method

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,19 @@
 module MoviesHelper
+
+  def performance(movie)
+    if movie.flop?
+      "Flop!"
+    else
+      number_to_currency(movie.total_gross, precision: 0)
+    end
+  end
+
+  def description(movie)
+    truncate(movie.description, length: 40, seperator: ' ')
+  end
+
+  def release_year(movie)
+    movie.released_on.year
+  end
+
 end
