93b44fbb0a9100fec30ad1dbc3d28d6956db3858
ts-oh <this.tsoh@gmail.com>
2023-07-21 21:12:29 +0900

    :busts_in_silhouette: [ADD]: user login and authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to @user, notice: "Signup successfull!"
     else
       render :new, status: :unprocessable_entity
@@ -37,7 +38,7 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-    redirect_to movies_url, status: :see_other, alert: 'Account succesfully deleted!'
+    redirect_to movies_url, status: :see_other, alert: "Account succesfully deleted!"
   end
 
 
