2922781649f7dca06db242bb333675a6710d77fd
ts-oh <this.tsoh@gmail.com>
2023-07-27 14:23:04 +0900

    :card_file_box: [CREATE]: m to m relation with user and review

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,21 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :reviews, dependent: :destroy
+
   validates :name, presence: true
 
   validates :email, presence: true,
-              format: { with: /\S+@\S+/ },
-              uniqueness: { case_sensitive: false }
+                    format: { with: /\S+@\S+/ },
+                    uniqueness: { case_sensitive: false }
 
   validates :password, length: { minimum: 10, allow_blank: true }
 
   validates :username, presence: true, format: {
-    with: /\A[A-Z0-9]+\z/i
-  }, uniqueness: {case_sensitive: false}
+               with: /\A[A-Z0-9]+\z/i,
+             }, uniqueness: { case_sensitive: false }
 
   def gravatar_id
     Digest::MD5::hexdigest(email.downcase)
   end
-
 end
